536c3e7bd80c3c51cf6c0ed5863b893508b0af75
Rudrani Deshpande <rudranid07@gmail.com>
2026-04-10 10:00:19 +0530

    Update kruskal.c

--- kruskal.c
+++ kruskal.c
@@ -1,56 +1,108 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX 100
 
+// Edge structure
 typedef struct {
     int u, v, w;
 } Edge;
 
 Edge edges[MAX];
+char locations[MAX][50];
 int parent[MAX];
 
+// Performance counters
+int findCount = 0;
+int unionCount = 0;
+
+// Get index of location name
+int getIndex(char name[], int n) {
+    for(int i = 0; i < n; i++) {
+        if(strcmp(locations[i], name) == 0)
+            return i;
+    }
+    return -1;
+}
+
+// Find with path compression
 int find(int x) {
+    findCount++;
     if(parent[x] == x)
         return x;
     return parent[x] = find(parent[x]);
 }
 
+// Union
 void unionSet(int a, int b) {
+    unionCount++;
     parent[a] = b;
 }
 
+// Sort edges by weight
 int compare(const void* a, const void* b) {
     return ((Edge*)a)->w - ((Edge*)b)->w;
 }
 
 int main() {
     int n, e;
+
+    printf("Enter number of locations and connections:\n");
     scanf("%d %d", &n, &e);
 
-    for(int i=0;i<e;i++)
-        scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].w);
+    printf("Enter location names:\n");
+    for(int i = 0; i < n; i++) {
+        scanf("%s", locations[i]);
+    }
 
-    for(int i=0;i<n;i++)
+    char loc1[50], loc2[50];
+
+    printf("Enter connections (location1 location2 cost):\n");
+    for(int i = 0; i < e; i++) {
+        scanf("%s %s %d", loc1, loc2, &edges[i].w);
+
+        edges[i].u = getIndex(loc1, n);
+        edges[i].v = getIndex(loc2, n);
+    }
+
+    // Initialize parent
+    for(int i = 0; i < n; i++) {
         parent[i] = i;
+    }
 
+    // Sort edges (important step)
     qsort(edges, e, sizeof(Edge), compare);
 
-    int cost = 0;
+    int totalCost = 0;
 
-    printf("Edges in MST:\n");
+    printf("\nSmart City Fiber Network Design\n");
+    printf("====================================\n");
+    printf("Selected Fiber Connections:\n");
 
-    for(int i=0;i<e;i++) {
+    for(int i = 0; i < e; i++) {
         int pu = find(edges[i].u);
         int pv = find(edges[i].v);
 
         if(pu != pv) {
-            printf("%d - %d : %d\n", edges[i].u, edges[i].v, edges[i].w);
-            cost += edges[i].w;
+            printf("%s <--> %s | Cost: %d\n",
+                   locations[edges[i].u],
+                   locations[edges[i].v],
+                   edges[i].w);
+
+            totalCost += edges[i].w;
             unionSet(pu, pv);
         }
     }
 
-    printf("Total Cost: %d\n", cost);
+    printf("------------------------------------\n");
+    printf("Minimum Total Fiber Cost: %d\n", totalCost);
+
+    // Performance Analysis
+    printf("\nPerformance Analysis:\n");
+    printf("Find operations: %d\n", findCount);
+    printf("Union operations: %d\n", unionCount);
+    printf("Time Complexity: O(E log E)\n");
+
     return 0;
 }
